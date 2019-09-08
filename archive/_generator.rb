cebc9473c7b46d59ec7869abd45ac6908ae345bf
Luna <lunacodes@gmail.com>
2019-09-08 19:31:28 -0400

    Attempt generate archives w/ jekyll-archive-page plugin

--- archive/_generator.rb
+++ archive/_generator.rb
@@ -3,18 +3,18 @@ MONTH_NAMES = ["", "January", "February", "March", "April", "May", "June", "July
 def write_template_file(path, permalink, title, options={})
     unless File.exists?(path)
         File.open(path, 'w') do |f|
-    #         f.puts "---"
-    #         f.puts "layout: archive"
-    #         f.puts "permalink: '#{permalink}'"
-    #         f.puts "redirect_from: 'archive/#{permalink}'"
-    #         f.puts "title: '#{title}'"
-    #         options.each do |k, v|
-    #             f.puts "#{k}: '#{v}'"
+            f.puts "---"
+            f.puts "layout: archive"
+            f.puts "permalink: '#{permalink}'"
+            f.puts "redirect_from: 'archive/#{permalink}'"
+            f.puts "title: '#{title}'"
+            options.each do |k, v|
+                f.puts "#{k}: '#{v}'"
             end
-    #         f.puts "---"
+            f.puts "---"
         end
-    #     puts "created archive page for #{title}"
-    # end
+        puts "created archive page for #{title}"
+    end
 end
 
 
