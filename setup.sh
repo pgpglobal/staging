c69f5af22813b6114272e232c4fc808706d3469e
Luna <lunacodes@gmail.com>
2019-11-06 02:51:29 -0500

    Update setup.sh to account for multiple branches & config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 # Don't mess with the CNAME File!!!!
 git update-index --skip-worktree CNAME
 
+# Fetch the _docs submodule (i.e. the PGPGlobal Wiki)
+git submodule update --init
+
 # Add Staging remote and fetch all changes from both remotes
 git remote add staging https://github.com/pgpglobal/staging.git
 git pull --all
@@ -8,10 +11,26 @@ git pull --all
 # Create and checkout local staging branch
 git checkout -b staging
 # Tell staging to track Remote staging:gh-pages
-gb -u staging/gh-pages
+git branch -u staging/gh-pages
+# Pull again - just to be on safe side
+git pull --no-edit
+# set `push.default` to always have staging push to staging:gh-pages.
+git config push.default upstream
 
-# Pull any changes for the new branch
-git pull --all
+# Create and/or switch to gh-pages and master branches
+# Pull any changes for the new branches
+git checkout gh-pages
+git branch -u origin/gh-pages
+git pull --no-edit
+# set `push.default` to always have gh-pages push to origin:gh-pages.
+git config push.default upstream
+
+git checkout -b master
+git branch -u origin/master
+git pull --no-edit
+
+# set `push.default` to always have master push to origin:master.
+git config push.default upstream
 
 # set `push.default` to always have staging push to staging:gh-pages.
 git config push.default upstream
